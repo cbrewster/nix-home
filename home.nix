8564091c657f1cb926b5ffb86f9eb7ccd31d60dd
Connor Brewster <cbrewster@hey.com>
2021-04-25 22:56:52 -0500

    Add direnv

--- home.nix
+++ home.nix
@@ -30,6 +30,12 @@
     _1password-gui
   ];
 
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    enableNixDirenvIntegration = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
