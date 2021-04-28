0beb7c8d28d629d17fb8db91949c135068a56240
Connor Brewster <cbrewster@hey.com>
2021-04-27 20:51:01 -0500

    Add p10k

--- home/zsh.nix
+++ home/zsh.nix
@@ -6,7 +6,6 @@
 
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
       plugins = [ "git" "sudo" ];
     };
 
@@ -15,6 +14,16 @@
     };
 
     plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
       {
         name = "zsh-autosuggestions";
         src = pkgs.fetchFromGitHub {
